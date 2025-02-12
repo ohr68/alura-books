c8fac11ebca87ef6c1418a942b52f0219e9bbcd1
Otávio Odenik <otavio.odenik@gmail.com>
2025-02-12 09:54:00 -0300

    adds favoritos route and requests

--- src/componentes/pesquisa/index.js
+++ src/componentes/pesquisa/index.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Input from "../input";
 import { useState, useEffect } from "react";
 import { getLivros } from "../../servicos/livros";
+import { postFavorito } from '../../servicos/favoritos';
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -57,6 +58,11 @@ function Pesquisa() {
         setLivros(livrosDaAPI);
     }
 
+    async function insertFavorito(id) {
+        await postFavorito(id);
+        alert(`Livro de id: ${id} inserido!`);
+    }
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
@@ -65,14 +71,12 @@ function Pesquisa() {
                 placeholder="Escreva sua próxima leitura"
                 onBlur={evento => {
                     const textoDigitado = evento.target.value;
-console.log(textoDigitado);
-
                     const resultadoPesquisa = livros.filter(livro => livro.nome.toLowerCase().includes(textoDigitado));
 
                     setLivrosPesquisados(resultadoPesquisa);
                 }} />
             {livrosPesquisados.map(livro => (
-                <Resultado>
+                <Resultado onClick={() => insertFavorito(livro.id)}>
                     <p>{livro.nome}</p>
                     <img src={livro.src} />
                 </Resultado>
--- src/rotas/Favoritos.js
+++ src/rotas/Favoritos.js
@@ -1,18 +1,80 @@
-import Pesquisa from '../componentes/pesquisa';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { getFavoritos, deleteFavorito } from '../servicos/favoritos';
+import livroImg from '../imagens/livro.png';
 
 const AppContainer = styled.div`
-   width: 100vw;
-   height: 100vh;
-   background-image: linear-gradient(90deg, #002F52 35%, #326589);
-`;
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+`
+
+const ResultadoContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
+    p {
+        width: 200px;
+        color: #FFF;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+const Titulo = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
+`
+
 
 function Favoritos() {
-    return (
-      <AppContainer>
-        <Pesquisa />
-      </AppContainer>
-    );
+  const [favoritos, setFavoritos] = useState([]);
+
+  async function fetchFavoritos() {
+    const favoritosDaAPI = await getFavoritos();
+    setFavoritos(favoritosDaAPI);
   }
-  
-  export default Favoritos
+
+  async function deletarFavorito(id) {
+    await deleteFavorito(id);
+    await fetchFavoritos();
+    alert(`Livro de id: ${id} deletado!`);
+  }
+
+  useEffect(() => {
+    fetchFavoritos()
+  }, []);
+
+  return (
+    <AppContainer>
+      <Titulo>Aqui estão seus livros favoritos:</Titulo>
+      <ResultadoContainer>
+        {favoritos.length !== 0 ? favoritos.map(favorito => (
+          <Resultado onClick={() => deletarFavorito(favorito.id)}>
+            <img src={livroImg} />
+            <p>{favorito.nome}</p>
+          </Resultado>
+        )) : null}
+      </ResultadoContainer>
+    </AppContainer>
+  );
+}
+
+export default Favoritos
